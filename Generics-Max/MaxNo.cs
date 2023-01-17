e80aaf7e177ce0510eee642a8f1732337526288d
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-17 14:42:17 +0530

    [Nehal]Adding: Finding Maximum with using class generics by extanding parameter

--- Generics-Max/MaxNo.cs
+++ Generics-Max/MaxNo.cs
@@ -8,35 +8,40 @@ namespace Generics_Max
     public class MaxNo<T> where T: IComparable
         
     {
-        T firstNo,secondNo, thirdNo;
-        public MaxNo(T FirstNo, T secondNo, T thirdNo) 
+        T firstNo,secondNo, thirdNo, forthNo;
+        public MaxNo(T FirstNo, T secondNo, T thirdNo, T ForthNo) 
         {
             this.firstNo = FirstNo;
             this.secondNo = secondNo;
             this.thirdNo = thirdNo;
+            this.forthNo = ForthNo;
         }
 
 
-        public static T Max  (T FirstNo, T SecondNo, T ThiredNo) 
+        public static T Max  (T FirstNo, T SecondNo, T ThiredNo, T ForthNo) 
         {
-            if (FirstNo.CompareTo(SecondNo) >0 && FirstNo.CompareTo(ThiredNo) >0)
+            if (FirstNo.CompareTo(SecondNo) > 0 && FirstNo.CompareTo(ThiredNo) > 0 && FirstNo.CompareTo(ForthNo) > 0)
             {
                 return FirstNo;
             }
-            if (SecondNo.CompareTo(FirstNo) >0 && SecondNo.CompareTo(ThiredNo) >0)
+            if (SecondNo.CompareTo(FirstNo) > 0 && SecondNo.CompareTo(ThiredNo) > 0 && SecondNo.CompareTo(ForthNo) > 0)
             {
                 return SecondNo;
             }
-            if(ThiredNo.CompareTo(FirstNo)>0 && ThiredNo.CompareTo(SecondNo) >0)
+            if (ThiredNo.CompareTo(FirstNo) > 0 && ThiredNo.CompareTo(SecondNo) > 0 && ThiredNo.CompareTo(ForthNo) > 0)
             {
                 return ThiredNo;
             }
-            throw new Exception(" FirstNo,SecondNo,ThiredNo is same");
+            if (ForthNo.CompareTo(FirstNo) > 0 && ForthNo.CompareTo(SecondNo) > 0 && ForthNo.CompareTo(ThiredNo) > 0)
+            {
+                return ForthNo;
+            }
+            throw new Exception(" FirstNo,SecondNo,ThiredNo, ForthNo is same");
         }
         
       public T check ()
         {
-            T max = MaxNo<T>.Max(this.firstNo, this.secondNo, this.thirdNo);
+            T max = MaxNo<T>.Max(this.firstNo, this.secondNo, this.thirdNo, this.forthNo);
                 return max;
         }
             
--- Generics-Max/Program.cs
+++ Generics-Max/Program.cs
@@ -6,9 +6,9 @@ namespace Generics_Max
     {
         static void Main(string[] args)
         {
-            MaxNo<int> a = new MaxNo<int>(314, 34, 4);
-            MaxNo<string> b = new MaxNo<string>("Apple", "Banana","Peach");
-            MaxNo<float> c = new MaxNo<float>(31.44f, 3481.43f, 412.44f);
+            MaxNo<int> a = new MaxNo<int>(314, 34, 4,7666);
+            MaxNo<string> b = new MaxNo<string>("Apple", "Banana","Peach","Zolo");
+            MaxNo<float> c = new MaxNo<float>(31.44f, 3481.43f, 412.44f, 23333.765f);
             Console.WriteLine(a.check());
             Console.WriteLine(b.check());
             Console.WriteLine(c.check());
